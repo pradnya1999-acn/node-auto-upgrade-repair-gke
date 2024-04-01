205d2ac606964cff2fcf4020b944fa1139c62feb
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-04-01 15:44:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+  initial_node_count       = 1
+  #enable_autopilot   = true
+
+  node_pool {
+    name = "default-node-pool"
+
+    node_config {
+      preemptible  = false
+      machine_type = "n1-standard-1"
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+    }
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
+  }
+
+}
