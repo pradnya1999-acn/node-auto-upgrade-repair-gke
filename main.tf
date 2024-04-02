e783eb53dfd5a987cceb5354612d03074a8cee82
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-04-02 17:51:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,35 +12,23 @@ resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
+  node_pool {
+    name = "default-node-pool"
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
+    node_config {
+      preemptible  = false
+      machine_type = "n1-standard-1"
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
     }
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-
-    tags = ["my-tags"]
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
   }
 
-  management {
-    auto_upgrade = true
-    auto_repair  = true
-  }
 }
