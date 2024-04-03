a259772833d92d64ba8ed1caa884545eb2f3640d
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-04-03 18:53:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
